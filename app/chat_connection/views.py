fa1015f4515fdafb119cc182366f7225004f0388
jestrelloso <jestrelloso@jairosoftcom.onmicrosoft.com>
2024-05-06 16:02:27 +0800

    added simple validations on chat connection view

--- app/chat_connection/views.py
+++ app/chat_connection/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Chat_Connection
+from core.models import Chat_Connection, Item
 from chat_connection import serializers
 
 
@@ -18,7 +18,13 @@ class ChatConnectionViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(sender=self.request.user)
+        receiver_id = self.request.data.get('receiver')  # fetch the user id in the req body
+        item_id = self.request.data.get('item')  # fetch the item id in the req body
+        items = Item.objects.filter(user=receiver_id)  # get all items under the user
+        for item in items:  # iterate in the items that is within that user
+            if str(item.id) == item_id:  # if it is equal to the item id in the req body
+                print('Found')  # print
+                serializer.save(sender=self.request.user)
 
     def get_queryset(self):
         """Retrieve chat connections for authenticated user."""
